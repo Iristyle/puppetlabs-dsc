17ad8c2eb4e317872586de4f08280c0c93c1e625
Ethan J. Brown <Iristyle@github>
2016-01-06 00:36:24 -0800

    more event cleanup

--- lib/puppet_x/puppetlabs/powershell_manager.rb
+++ lib/puppet_x/puppetlabs/powershell_manager.rb
@@ -17,11 +17,12 @@ module PuppetX::Dsc
       @output_ready_event_name =  "Global\\#{SecureRandom.uuid}"
       @output_ready_event = create_event(@output_ready_event_name)
 
-      @stdin, @stdout, @stderr, @wait_thr = Open3.popen3(cmd)
+      @stdin, @stdout, @stderr, @ps_process = Open3.popen3(cmd)
       @stdin.sync = true
       @stdout.sync = true
+      @stderr.sync = true
 
-      Puppet.debug "#{Time.now} #{cmd} is running as pid: #{@wait_thr[:pid]}"
+      Puppet.debug "#{Time.now} #{cmd} is running as pid: #{@ps_process[:pid]}"
 
       at_exit { exit }
     end
@@ -29,11 +30,6 @@ module PuppetX::Dsc
     def self.is_readable?(stream, timeout = 0.5)
       read_ready = IO.select([stream], [], [], timeout)
       read_ready && stream == read_ready[0][0]
-    end
-
-    def self.is_writable?(stream, timeout = 2)
-      write_ready = IO.select([], [stream], [], timeout)
-      write_ready && stream == write_ready[1][0]
     end
 
     def execute(powershell_code)
@@ -64,8 +60,8 @@ module PuppetX::Dsc
       FFI::WIN32.CloseHandle(@output_ready_event) if @output_ready_event
 
       # TODO: kill if not exited in X seconds?
-      # @return_code = Process::waitpid(@wait_thr[:pid], 2)
-      @return_code = @wait_thr.value
+      # @return_code = Process::waitpid(@ps_process[:pid], 2)
+      @return_code = @ps_process.value
     end
 
     private
@@ -88,8 +84,12 @@ module PuppetX::Dsc
       handle
     end
 
-    # The specified object is a mutex object that was not released by the thread that owned the mutex object before the owning thread terminated. Ownership of the mutex object is granted to the calling thread and the mutex state is set to nonsignaled.
-    # If the mutex was protecting persistent state information, you should check it for consistency.
+    # The specified object is a mutex object that was not released by the thread
+    # that owned the mutex object before the owning thread terminated. Ownership
+    # of the mutex object is granted to the calling thread and the mutex state
+    # is set to nonsignaled.
+    # If the mutex was protecting persistent state information, you should check
+    # it for consistency.
     WAIT_ABANDONED = 0x00000080
     # The state of the specified object is signaled.
     WAIT_OBJECT_0 = 0x00000000
@@ -98,20 +98,19 @@ module PuppetX::Dsc
     # Complete failure
     WAIT_FAILED = 0xFFFFFFFF
 
-    def wait_on(wait_object, milliseconds = 20 * 1000)
-      # wait 200ms at a time until signaled
+    def wait_on(wait_object, timeout = 20 * 1000)
       total = 0
       wait_ms = 50
       while true
         wait_result = Puppet::Util::Windows::Process::WaitForSingleObject(wait_object, wait_ms)
         case wait_result
         when WAIT_OBJECT_0
-          Puppet.debug "Object signaled - Ruby process holds mutex"
+          Puppet.debug "Object signaled"
           return
         when WAIT_TIMEOUT
           total += wait_ms
-          Puppet.debug "Waiting #{wait_ms} milliseconds (total wait #{total})..."
-          raise 'Wuh-oh wait exceeded' if (total > milliseconds)
+          Puppet.debug "Waiting #{wait_ms} timeout (total wait #{total})..."
+          raise 'Wuh-oh wait exceeded' if (total > timeout)
         when WAIT_ABANDONED
           Puppet.debug "Wait abandoned..."
           raise 'Wuh-oh buddy...'
@@ -122,8 +121,6 @@ module PuppetX::Dsc
     end
 
     def write_stdin(input)
-      raise "Unwriteable stream" if !self.class.is_writable?(@stdin)
-
       @stdin.puts(input)
     rescue => e
       Puppet.warning "Error writing STDIN / reading STDOUT: #{e}"
@@ -136,8 +133,6 @@ module PuppetX::Dsc
       initially_readable = false
       while self.class.is_readable?(@stdout, 0.1) do
         initially_readable = true
-        # this is necessary to ensure all output captured
-        # @stdout.flush()
 
         l = @stdout.gets
         Puppet.debug "#{Time.now} STDOUT> #{l}"
